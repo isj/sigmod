4f1b98e4329c9058d42fbb228c400aaab6197eb5
foundry <foundry>
2013-12-02 12:34:04 +0000

    minor adjustments

--- code-foundry/sigmod/sigmod/isj_impl/core.cpp
+++ code-foundry/sigmod/sigmod/isj_impl/core.cpp
@@ -274,6 +274,52 @@ const char *bitap_bitwise_search(char* string_a, int length_a, char* string_b, i
     return NULL;
 }
 
+const char *bitap_fuzzy_bitwise_search(const char *text, const char *pattern, int k)
+{
+    const char *result = NULL;
+    int m = strlen(pattern);
+    unsigned long *R;
+    unsigned long pattern_mask[CHAR_MAX+1];
+    int i, d;
+
+    if (pattern[0] == '\0') return text;
+    if (m > 31) return "The pattern is too long!";
+
+    /* Initialize the bit array R */
+    R =  malloc((k+1) * sizeof (*R));
+    for (i=0; i <= k; ++i)
+        R[i] = ~1;
+
+    /* Initialize the pattern bitmasks */
+    for (i=0; i <= CHAR_MAX; ++i)
+        pattern_mask[i] = ~0;
+    for (i=0; i < m; ++i)
+        pattern_mask[pattern[i]] &= ~(1UL << i);
+
+    for (i=0; text[i] != '\0'; ++i) {
+        /* Update the bit arrays */
+        unsigned long old_Rd1 = R[0];
+
+        R[0] |= pattern_mask[text[i]];
+        R[0] <<= 1;
+
+        for (d=1; d <= k; ++d) {
+            unsigned long tmp = R[d];
+            /* Substitution is all we care about */
+            R[d] = (old_Rd1 & (R[d] | pattern_mask[text[i]])) << 1;
+            old_Rd1 = tmp;
+        }
+
+        if (0 == (R[k] & (1UL << m))) {
+            result = (text+i - m) + 1;
+            break;
+        }
+    }
+
+    free(R);
+    return result;
+}
+
 // Computes edit distance between a null-terminated string "a" with length "na"
 //  and a null-terminated string "b" with length "nb" 
 int EditDistance(char* string_a, int length_a, char* string_b, int length_b)
